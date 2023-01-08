6b5e582372bc602fa7dd8f55833846e4bbc77f91
Waldemar Schott <42361+wschott@users.noreply.github.com>
2023-01-08 10:17:42 +0100

    Add custom error class.

--- src/apit/error.py
+++ src/apit/error.py
@@ -7,3 +7,8 @@ class ApitStoreConnectionError(ApitError):
         super().__init__(
             f"Connection to Apple Music/iTunes Store failed due to error: ${error_message}"
         )
+
+
+class ApitNoTagsPresentError(ApitError):
+    def __init__(self) -> None:
+        super().__init__("No tags present")
--- src/apit/tagging/read.py
+++ src/apit/tagging/read.py
@@ -5,6 +5,7 @@ import mutagen.mp4
 from apit.commands.show.reporting.file_tags import FileTags
 from apit.commands.show.reporting.mp4.mp4_tag import Mp4Tag
 from apit.error import ApitError
+from apit.error import ApitNoTagsPresentError
 from apit.store.constants import BLACKLIST
 from apit.tag_id import TagId
 
@@ -18,7 +19,7 @@ def read_metadata(file: Path) -> mutagen.mp4.MP4:
 
 def to_file_tags(metadata_file: mutagen.mp4.MP4) -> FileTags:
     if not metadata_file.tags:
-        raise ApitError("No tags present")
+        raise ApitNoTagsPresentError()
 
     return FileTags(
         [Mp4Tag(TagId(tag), value) for tag, value in metadata_file.tags.items()]
@@ -29,7 +30,7 @@ def is_itunes_bought_file(file: Path) -> bool:
     try:
         mp4_file = read_metadata(file)
         if not mp4_file.tags:
-            raise ApitError("No tags present")
+            raise ApitNoTagsPresentError()
     except ApitError:
         return False
     else:
