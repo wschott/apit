08fa9fee1f4bd5ce8ab055ca406585f1b541e623
Waldemar Schott <42361+wschott@users.noreply.github.com>
2023-01-16 22:13:32 +0100

    Refactor.

--- src/apit/tagging/mp4/constants.py
+++ src/apit/tagging/mp4/constants.py
@@ -14,16 +14,9 @@ class MP4_MAPPING(TagIdEnum):
     COPYRIGHT       = 'cprt'
     TRACK_NUMBER    = 'trkn'
     DISC_NUMBER     = 'disk'
-    RATING          = 'rtng'  # TODO int
-    MEDIA_TYPE      = 'stik'  # TODO int
-    CONTENT_ID      = 'cnID'  # catalog ID
     COMPILATION     = 'cpil'  # bool
     ARTWORK         = 'covr'
 
-    OWNER_NAME      = 'ownr'
-    USER_MAIL       = 'apID'
-
-    # TODO unused for now
     GAPLESS         = 'pgap'
     BPM             = 'tmpo'
     COMPOSER        = '\xa9wrt'
@@ -31,18 +24,26 @@ class MP4_MAPPING(TagIdEnum):
     GROUPING        = '\xa9grp'
     TOOL            = '\xa9too'
     LYRICS          = '\xa9lyr'
-    PURCHASE_DATE   = 'purd'
-
-    PLAYLIST_ID     = 'plID'
-    ARTIST_ID       = 'atID'
-    GENRE_ID        = 'geID'
-    COMPOSER_ID     = 'cmID'  # really composer id?
-    ISRC_ID         = 'xid '  # yes, with a space at the end; mixture of "{Record_Label_Name}:isrc:{ISRC_SONG_CODE}"
-    STOREFRONT_ID   = 'sfID'
 
     SORT_ORDER_TITLE = 'sonm'
     SORT_ORDER_ARTIST = 'soar'
     SORT_ORDER_ALBUM = 'soal'
     SORT_ORDER_ALBUM_ARTIST = 'soaa'
     SORT_ORDER_COMPOSER = 'soco'
+
+    # format specific values
+    RATING          = 'rtng'  # TODO int
+    MEDIA_TYPE      = 'stik'  # TODO int
+    CONTENT_ID      = 'cnID'  # catalog ID
+    OWNER_NAME      = 'ownr'
+    USER_MAIL       = 'apID'
+    PURCHASE_DATE   = 'purd'
+
+    PLAYLIST_ID = 'plID'
+    ARTIST_ID = 'atID'
+    GENRE_ID = 'geID'
+    COMPOSER_ID = 'cmID'  # really composer id?
+    ISRC_ID = 'xid '  # yes, with a space at the end; mixture of "{Record_Label_Name}:isrc:{ISRC_SONG_CODE}"
+    STOREFRONT_ID = 'sfID'
+
     # fmt: on
