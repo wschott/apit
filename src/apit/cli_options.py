6649cdaf1e4f2573224c235dcbde0c1fe40e4330
Waldemar Schott <42361+wschott@users.noreply.github.com>
2023-01-20 22:03:26 +0100

    Remove unnecessary option in type.

--- src/apit/cli_options.py
+++ src/apit/cli_options.py
@@ -13,7 +13,6 @@ class CliOptions(Namespace):
     command: str
     verbose_level: int
     has_backup_flag: bool
-    has_search_result_cache_flag: bool
     has_embed_artwork_flag: bool
     artwork_size: int
     path: Path
